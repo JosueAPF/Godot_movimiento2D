2db93e4f2b2950199c94f059b5111eb56f6f20de
josue <josueporras124@gmail.com>
2024-07-03 18:13:40 -0600

    se corriguieron scripts de bloques dinamicos y se agregaron bloques estaticaso

--- Player.gd
+++ Player.gd
@@ -1,24 +1,18 @@
 extends KinematicBody2D
 
-# Velocidad del personaje
-var speed = 400
+const SPEED = 200
 
 func _process(delta):
-	# Vector de movimiento inicializado a cero
-	var motion = Vector2()
-
-	# Capturar la entrada del teclado
+	var velocity = Vector2()
+	
 	if Input.is_action_pressed("ui_right"):
-		motion.x += 1
+		velocity.x += 1
 	if Input.is_action_pressed("ui_left"):
-		motion.x -= 1
+		velocity.x -= 1
 	if Input.is_action_pressed("ui_down"):
-		motion.y += 1
+		velocity.y += 1
 	if Input.is_action_pressed("ui_up"):
-		motion.y -= 1
+		velocity.y -= 1
 
-	# Normalizar el vector de movimiento y multiplicar por la velocidad
-	motion = motion.normalized() * speed
-
-	# Mover el personaje usando move_and_slide
-	move_and_slide(motion)
+	velocity = velocity.normalized() * SPEED
+	move_and_slide(velocity)
